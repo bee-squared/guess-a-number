69902d484de457c15deecd7fa7fc6e6ca2ec3925
Brian Bouchard <b.bouch@gmail.com>
2020-04-06 14:58:38 -0600

    render header on app

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
-const Header = () => {
+const Header = (props) => {
   return (
     <View style={styles.header}>
       <Text style={styles.headerTitle}>{props.title}</Text>
@@ -11,14 +11,16 @@ const Header = () => {
 
 const styles = StyleSheet.create({
   header: {
-    width: '100%';
-    height: 90;
-    paddingTop: 36;
-    backgroundColor: '#336699';
-    alignItems: center;
+    width: '100%',
+    height: 90,
+    paddingTop: 36,
+    backgroundColor: '#336699',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   headerTitle: {
-
+    color: 'black',
+    fontSize: 18,
   }
 });
 
