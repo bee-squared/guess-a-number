90cfb7eae5bd622b44276e0fb80f9e62d6b16284
Brian Bouchard <b.bouch@gmail.com>
2020-04-07 09:18:04 -0600

    add card view to input container and create shell component for card

---
+++ components/Card.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View } from 'react-native';
+
+const Card = (props) => {
+  return (
+
+  )
+}
+
+const styles = StyleSheet({});
+
+export default Card;
--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, TextInput, StyleSheet, Button } from 'react-native';
+import { View, Text, TextInput, StyleSheet, Button, ShadowPropTypesIOS } from 'react-native';
 
 const StartGameScreen = (props) => {
   return (
@@ -7,10 +7,14 @@ const StartGameScreen = (props) => {
       <Text style={styles.title}>Start a New Game!</Text>
       <View style={styles.inputContainer}>
         <Text style={styles.instruction}>Select a Number</Text>
-        <TextInput />
+        <TextInput style={styles.input}/>
         <View style={styles.buttonContainer}>
-          <Button title='Reset' onPress={() => {}} />
-          <Button title='Confirm' onPress={() => {}} />
+          <View style={styles.button}>
+            <Button title='Reset' onPress={() => {}} />
+          </View>
+          <View style={styles.button}>
+            <Button title='Confirm' onPress={() => {}} />
+          </View>
         </View>
       </View>
     </View>
@@ -19,7 +23,7 @@ const StartGameScreen = (props) => {
 
 const styles = StyleSheet.create({
   screen: {
-    flex: 1, // takes all the space it can get, both vertically and horizontally
+    // flex: 1, // takes all the space it can get, both vertically and horizontally
     alignItems: 'center',
     justifyContent: 'center',
     padding: 20,
@@ -31,21 +35,44 @@ const styles = StyleSheet.create({
   },
   instruction: {
     color: 'black',
-    padding: 30,
-    marginVertical: 10,
+    marginBottom: 20,
     fontSize: 20,
   },
   inputContainer: {
+    justifyContent: 'center',
     width: 300,
     maxWidth: '80%',
     alignItems: 'center',
+    shadowColor: 'black',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowRadius: 2,
+    shadowOpacity: 0.26,
+    backgroundColor: 'white',
+    height: 240,
+    elevation: 4,
+    padding: 20,
+    borderRadius: 10,
+  },
+  input: {
+    width: '90%',
+    height: 40,
+    borderColor: 'black',
+    borderWidth: 1,
+    fontSize: 18,
+    marginBottom: 20,
   },
   buttonContainer: {
     flexDirection: 'row',
     width: '100%',
     justifyContent: "space-between",
-    paddingHorizontal: 15,
+    paddingHorizontal: 20,
     height: 50,
+  },
+  button: {
+    width: 100,
   }
 });
 
