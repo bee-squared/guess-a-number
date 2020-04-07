202877391c0b0c3f547e8b495dfaad6bbd1ed079
Brian Bouchard <b.bouch@gmail.com>
2020-04-07 09:22:16 -0600

    modify card styles to accept new and incoming styles from props

--- components/Card.js
+++ components/Card.js
@@ -2,11 +2,24 @@ import React from 'react';
 import { View } from 'react-native';
 
 const Card = (props) => {
-  return (
-
-  )
+  return <View style={{...styles.card, ..props.style}}>{props.children}</View>
 }
 
-const styles = StyleSheet({});
+const styles = StyleSheet({
+  card: {
+    shadowColor: 'black',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowRadius: 2,
+    shadowOpacity: 0.26,
+    backgroundColor: 'white',
+    height: 240,
+    elevation: 4,
+    padding: 20,
+    borderRadius: 10,
+  }
+});
 
 export default Card;
