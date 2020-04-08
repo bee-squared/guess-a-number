d92d714043e6656086fcec2c022961fadc39570b
Brian Bouchard <b.bouch@gmail.com>
2020-04-07 23:15:06 -0600

    add styling to screen and button container for GameScreen screen/component

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -18,10 +18,10 @@ const GameScreen = (props) => {
   const [currentGuess, setCurrentGuess] = useState(generateRandomBetween(1,100,props.userChoice))
 
   return (
-    <View>
+    <View style={styles.screen}>
       <Text>Opponent's Guess</Text>
       <NumberContainer>{currentGuess}</NumberContainer>
-      <Card>
+      <Card style={styles.buttonContainer}>
         <Button title='LOWER'/>
         <Button title='GREATER'/>
       </Card>
@@ -31,7 +31,18 @@ const GameScreen = (props) => {
 }
 
 const styles = StyleSheet.create({
-
+  screen: {
+    flex: 1,
+    padding: 10,
+    alignItems: 'center',
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    marginTop: 20,
+    width: 300,
+    maxWidth: '80%'',
+  }
 })
 
 export default GameScreen;
